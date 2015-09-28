215979f95f1674029a812b3c856ae9e15f6346f0
Giovanni Pizzoferrato <Gio@adminisatorsmbp.dhcp.bngf.local>
2015-09-28 13:53:58 -0400

    Initial Commit

---
+++ Record Keeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Record Keeper
+//
+//  Created by Giovanni Pizzoferrato on 2015-09-28.
+//  Copyright © 2015 Giovanni Pizzoferrato. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
